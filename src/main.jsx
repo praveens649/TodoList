2a029162fa4eaebefe926927f88852e5699b9eb2
praveens649 <praveenv649@gmail.com>
2025-05-31 20:07:38 +0530

    minor bug

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import Footer from "./components/Footer.jsx";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
